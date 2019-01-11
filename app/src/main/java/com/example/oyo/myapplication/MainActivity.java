119ef05f99d04169f3d2208872973acac1cc9661
oyo <oyo@Administrators-MacBook-Pro.local>
2019-01-11 13:33:54 +0530

    FirstCommit

---
+++ app/src/main/java/com/example/oyo/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.oyo.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MYAPP","oncreate");
+    }
+
+
+    public void sendMessageButton(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vogella.com"));
+        startActivity(i);
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(i);
+    }
+}
