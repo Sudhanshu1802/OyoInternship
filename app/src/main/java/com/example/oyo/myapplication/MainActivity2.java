f6b967da2a12f04246d10d44bc53c919630b0cfd
oyo <oyo@Administrators-MacBook-Pro.local>
2019-01-16 22:21:38 +0530

    fcm and sqLite completed

--- app/src/main/java/com/example/oyo/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/oyo/myapplication/MainActivity2.java
@@ -1,38 +1,57 @@
 package com.example.oyo.myapplication;
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity2 extends Activity {
     public int t = 3;
     public static final String URL = "https://api.github.com/users";
     private Context mContext;
+    public EditText editFname,editLname,editEmail,editMob,editID;
+    public Button btn,view_btn,update_btn,delete_btn;
+    public MyDatabase myDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Button button = findViewById(R.id.button8);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences prefs = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
-                String token = prefs.getString("regId", null);
+        myDatabase = new MyDatabase(this);
+        editFname = findViewById(R.id.editText3);
+        editLname = findViewById(R.id.editText4);
+        editEmail = findViewById(R.id.editText5);
+        editMob = findViewById(R.id.editText6);
+        editID = findViewById(R.id.editText7);
+        btn = findViewById(R.id.button8);
+        view_btn = findViewById(R.id.view_data);
+        update_btn = findViewById(R.id.update_data);
+        delete_btn = findViewById(R.id.delete_data);
+        setListener();
+        viewData();
+        updateData();
 
-                if(!TextUtils.isEmpty(token)){
-                Toast.makeText(MainActivity2.this,"FCM Token: "+token,Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(MainActivity2.this,"FCM Token NULL",Toast.LENGTH_LONG).show();
-                }
-                Log.i("Token is ", "Token: "+ token);
-            }
-        });
+        deleteData();
+
+//        Button button = findViewById(R.id.button8);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SharedPreferences prefs = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+//                String token = prefs.getString("regId", null);
+//
+//                if(!TextUtils.isEmpty(token)){
+//                Toast.makeText(MainActivity2.this,"FCM Token: "+token,Toast.LENGTH_LONG).show();
+//                }else{
+//                    Toast.makeText(MainActivity2.this,"FCM Token NULL",Toast.LENGTH_LONG).show();
+//                }
+//                Log.i("Token is ", "Token: "+ token);
+//            }
+//        });
 //        final Handler myHandler = new Handler();
 //        myHandler.post(new Runnable() {
 //            @Override
@@ -48,6 +67,76 @@ public class MainActivity2 extends Activity {
 //        });
 
 
+    }
+
+    private void deleteData() {
+        delete_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myDatabase.DeleteEntry(editID.getText().toString());
+                    }
+                }
+        );
+    }
+
+    private void viewData() {
+        view_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDatabase.viewData();
+                        if(res.getCount()==0) return;
+                        StringBuffer stringBuffer = new StringBuffer();
+                        while(res.moveToNext()){
+                            stringBuffer.append("Id : "+ res.getString(0)+"\n");
+                            stringBuffer.append("First Name : "+ res.getString(1)+"\n");
+                            stringBuffer.append("Last Name : "+ res.getString(2)+"\n");
+                            stringBuffer.append("Email : "+ res.getString(3)+"\n");
+                            stringBuffer.append("Mobile : "+ res.getString(4)+"\n\n");
+                        }
+                        showFetchedData("DATA", stringBuffer.toString());
+                    }
+                }
+        );
+    }
+    public void showFetchedData(String title,String text){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(text);
+        builder.show();
+
+    }
+    private void updateData(){
+        update_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                            myDatabase.UpdateData(editID.getText().toString(),
+                                    editFname.getText().toString(),
+                                    editLname.getText().toString(),
+                                    editEmail.getText().toString(),
+                                    editMob.getText().toString()
+                            );
+                    }
+                }
+        );
+
+    }
+
+    private void setListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = myDatabase.insertData(editFname.getText().toString(),
+                        editLname.getText().toString(),
+                        editEmail.getText().toString(),
+                        editMob.getText().toString());
+                if(isInserted==true) Toast.makeText(MainActivity2.this,"Data Added",Toast.LENGTH_LONG).show();
+                else  Toast.makeText(MainActivity2.this,"Error",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void storeRegIdInPref(String token) {
--- app/src/main/java/com/example/oyo/myapplication/MyDatabase.java
+++ app/src/main/java/com/example/oyo/myapplication/MyDatabase.java
@@ -1,6 +1,8 @@
 package com.example.oyo.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -22,7 +24,7 @@ public class MyDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " +TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,FIRST_NAME TEXT, LAST_NAME TEXT,EMAIL TEXT,MOB NUMBER )");
+        db.execSQL("create table " +TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,FIRST_NAME TEXT, LAST_NAME TEXT,EMAIL TEXT,MOB TEXT )");
 
     }
 
@@ -30,5 +32,38 @@ public class MyDatabase extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists "+ TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String fname, String lname, String email, String Mob){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,fname);
+        contentValues.put(COL3,lname);
+        contentValues.put(COL4,email);
+        contentValues.put(COL5, Mob);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if(result==-1) return false;
+        else return true;
+    }
+    public Cursor viewData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("select * from " + TABLE_NAME,null);
+        return data;
+    }
+    public void UpdateData(String id,String fname, String lname, String email, String Mob){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL1,id);
+        contentValues.put(COL2,fname);
+        contentValues.put(COL3,lname);
+        contentValues.put(COL4,email);
+        contentValues.put(COL5, Mob);
+        db.update(TABLE_NAME,contentValues,"id=?",new String[] {id});
+
+    }
+    public void DeleteEntry(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME,"ID = ?",new String[] {id});
+
     }
 }
